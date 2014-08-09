ddca1c650ad3c5094ff3a541f9ac12006b1b9059
ZhengYingHao <zyh1010110@163.com>
2014-08-09 08:28:39 +0800

    解决增、删、绘制子弹时出现的异常

--- planeGame/PlanePanel.java
+++ planeGame/PlanePanel.java
@@ -16,11 +16,13 @@ public class PlanePanel extends JPanel{
 	private ArrayList<Plane> planes;
 	private static ArrayList<Bullet> bullets;
 	private static ArrayList<Bullet> removeList;
+	private static ArrayList<Bullet> addList;
 	
 	public PlanePanel() {
 		planes = new ArrayList<Plane>();
 		bullets = new ArrayList<Bullet>();
 		removeList = new ArrayList<Bullet>();
+		addList = new ArrayList<Bullet>();
 	}
 	
 	@Override
@@ -30,18 +32,29 @@ public class PlanePanel extends JPanel{
 		//下面有问题，若 this 改为 teString， 程序运行马上出错。		---- NULL POINTER， teString没有赋值！
 		//若不改，子弹太多， 	 for (Bullet b : bullets) {  里句出错
 		//应该系迭代器问题，子弹随时会删除 or 增加，而里度又重新绘制，出现矛盾
+		//使用了两个数组列表，一个用于add，一个用于remove，暂时解决出现exception的问题。
+		//熟悉线程与锁之间的关系后再优化
 		synchronized (bullets) {
 			g2.drawImage(p.getImage(), (int)p.getX(), (int)p.getY(), Plane.X_SIZE, Plane.Y_SIZE, null);
-			for (Bullet bullet : removeList)
+			for (Bullet bullet : addList) {
+				bullets.add(bullet);
+				//这样删除是会直接出现错误的！
+//				addList.remove(bullet);
+			}
+			for (Bullet bullet : removeList) {
 				bullets.remove(bullet);
+//				removeList.remove(bullet);
+			}
 			for (Bullet b : bullets) {
 				g2.fill(b.getShape());
 			}
+			addList = new ArrayList<Bullet>();
+			removeList = new ArrayList<Bullet>();
 		}
 	}
 	
 	public synchronized void addBullet(Bullet b) {
-		bullets.add(b);
+		addList.add(b);
 	}
 	public void addPlane(Plane plane) {
 		this.p = plane;
